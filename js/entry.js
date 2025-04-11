4d8d93819da801d71161f95792db363a8d1f6bdb
Lily Madojemu <lmadojem@umich.edu>
2025-04-10 22:10:31 -0400

    Structure a single journal entry page

--- js/entry.js
+++ js/entry.js
@@ -12,7 +12,6 @@ const journalEntry = document.querySelector("#entryContainer")
  * and anytime the memory list changes.
  */
 
-// Use entryList, try without restating first
 const renderEntry = () => {
     journalEntry.innerHTML="";
 
@@ -25,19 +24,19 @@ const renderEntry = () => {
   // •	On the entry page, use the ID from the URL to fetch the correct entry data from localStorage and display it.
 
 
-    const sortedMemories = memoryList.sort((a, b) => a.date - b.date);
-    localStorage.setItem('memory.list', JSON.stringify(sortedMemories));
+    // get item from local storage based on URL parameter ( this should be supplied to us already)
     
-    // Create the html structure of the entry
-    entryList.forEach((entry) => {
-      memoryContainer.innerHTML += `<div class="position-relative col-12 border border-secondary rounded my-3 p-3 bg-white">
-       <div class="d-flex">
-         <h3>${memory.title}</h3>
-         <small class="px-1 text-muted align-self-center">${formatDateForMemory(memory.date)}</small>
-       </div>
-     <button data-date="${memory.date}" class="close-button">Ⓧ</button>
-     <p>${memory.description}</p>
-     </div>`;
-    });
+
+    // Update to reflect html structure of journal entry and insert where info for an individual entry would go in it
+    journalEntry.innerHTML += 
+    `<div class="position-relative col-12 border border-secondary rounded my-3 p-3 bg-white">
+      <div class="d-flex">
+        <h3>${memory.title}</h3>
+        <small class="px-1 text-muted align-self-center">${formatDateForMemory(memory.date)}</small>
+      </div>
+    <button data-date="${memory.date}" class="close-button">Ⓧ</button>
+    <p>${memory.description}</p>
+    </div>`;
+    ;
   };
 
